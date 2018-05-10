a6cc22bbced86340a2f60fd4e2ecbd3ac64e1d8c
HossamHassan47 <hossam.hassan@outlook.com>
2018-05-10 13:38:33 +0200

    call

--- app/src/main/java/com/wordpress/hossamhassan47/nannasarts/HomeActivity.java
+++ app/src/main/java/com/wordpress/hossamhassan47/nannasarts/HomeActivity.java
@@ -1,23 +1,18 @@
 package com.wordpress.hossamhassan47.nannasarts;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
